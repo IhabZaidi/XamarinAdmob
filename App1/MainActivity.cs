7aefe9ade34e45f01aca5b5ec0a173c7f4a4371f
IhabZaidi <ihab.bibou@gmail.com>
2016-07-01 00:41:01 +0100

    first commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Ads;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            AdView mAdView = FindViewById<AdView>(Resource.Id.adView1);
+            var adRequest = new AdRequest.Builder().Build();
+            // Start loading the ad.
+            mAdView.LoadAd(adRequest);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
